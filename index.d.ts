442a3b0f22ddc5179268b949ac386790cf76f156
Eduardo Henrique <eduardohds.dev@outlook.com>
2022-01-02 12:10:25 -0300

    Add types

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+import { ReactNode, ReactElement } from 'react';
+
+type SwitchProps = {
+	fallback: ReactNode;
+	children: ReactNode;
+};
+
+type MatchProps = {
+	when: boolean;
+	children: ReactNode;
+};
+
+export function Switch(props: SwitchProps): ReactElement | null;
+
+export function Match(props: MatchProps): ReactElement | null;
